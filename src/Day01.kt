910ec8f0b6f64e4fa27c53189babe12e2e1f030b
Joseph Ottinger <joeo@enigmastation.com>
2022-12-01 15:42:29 -0500

    Advent of code day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+data class Elf(val calories: List<Int>) {
+    fun totalCalories(): Int {
+        return calories.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    override fun toString(): String {
+        return calories.toString()+" (total: "+totalCalories()+")"
     }
+}
+
+fun main() {
+    val mode = "actual"
+
+    fun convertToElves(input: List<String>): List<Elf> {
+        val elves: MutableList<Elf> = mutableListOf()
+        val calories: MutableList<Int> = mutableListOf()
+        input.stream().forEach {
+            if (it.trim() == "") {
+                elves += Elf(calories.toList())
+                calories.clear()
+            } else {
+                calories+=it.trim().toInt()
+            }
+        }
+        return elves
+    }
+
+    fun part1(input: List<Elf>): Int {
+        return input
+            .map { it.totalCalories() }
+            .maxOf { it }
+    }
+
+    fun part2(input: List<Elf>): Int {
+        return input
+            .map { it.totalCalories() }
+            .sorted()
+            .reversed()
+            .subList(0,3)
+            .sum()
+    }
+
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput(mode)
+//    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput(mode)
+    val elves= convertToElves(input)
+
+    println("the calories from the top elf        : "+part1(elves))
+    println("the calories from the top three elves: "+part2(elves))
 }
